02ee1ad15f8a415a40ab63d176c9d9d123233dce
hemanth <hemanth@gideon.home>
2020-08-24 19:26:59 +0100

    Updates

---
+++ precache-manifest.17d85a2a4cd1bfeac1f10d42ef79fbdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0c8e391bf710611e8498b38c85b0b2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a35149165ca25759291b",
+    "url": "/portfolio/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35149165ca25759291b",
+    "url": "/portfolio/static/js/main.8c6e3952.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
